b62e5152b3701f465ab8fe689cf836f711d8c772
royeradames <royeraadames@gmail.com>
2020-08-14 12:35:13 -0400

    finish logic for form.js

--- smurfs/src/components/Form.js
+++ smurfs/src/components/Form.js
@@ -1,25 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 //redux
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
-export function Form(){
-    return(
+export function Form(props) {
+    const [user, setUser] = useState({
+        name: "",
+        age: '',
+        height: "",
+    })
+    const handleChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        props.addUser(user)
+        setUser({
+            name: "",
+            age: '',
+            height: "",
+        })
+    }
+    return (
         <section>
             <h2>
                 Become a new membera
             </h2>
-            <form onSubmit={''}>
-                <input placeholder='name'/>
-                <input placeholder='age'/>
-                <input placeholder='height'/>
-                <button type='submit'>Submit</button>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    name='name'
+                    placeholder='Name'
+                    onChange={handleChange}
+                    value={user.name}
+                />
+                <input 
+                    name='age'
+                    placeholder='Age'
+                    onChange={handleChange}
+                    value={user.age}
+                />
+                <input 
+                    name='height'
+                    placeholder='Height'
+                    onChange={handleChange}
+                    value={user.height}
+                />
+                <button type='submit'>Become New Member</button>
             </form>
         </section>
     )
 }
 
-const mapStateToProps = (state) =>{
-    return{
+const mapStateToProps = (state) => {
+    return {
         state
     }
 }
