aa398802782abeac3467965eee598cf0344dd811
royeradames <royeraadames@gmail.com>
2020-08-14 14:00:58 -0400

    still testing

--- smurfs/src/components/App.test.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { render, screen, fireEvent, act} from '@testing-library/react'
-import App from './App'
-
-test('Renders App without errors', () => {
-    render(<App />)
-})
